ebde6fa62a0cd74e2d2cbebf0a9cd7f0b544dad5
2405-011-pw <walypa56@gmail.com>
2024-08-07 14:27:34 +0100

    post request with a few error checks

--- src/server.js
+++ src/server.js
@@ -13,6 +13,43 @@ app.use(cors());
 //parse body text of requests having content-type application/json, attaching result to `req.body`
 app.use(express.json());
 
+app.get("/api/coffeeData/", (request, response) => {
+    response.json(coffeeData);
+});
+
+app.post("/api/coffeeData", (request, response) => {
+    try {
+        const name = request.body.name;
+        if (!name) {
+            throw new Error("Name field is required");
+        }
+
+        if (!(typeof name === "string")) {
+            throw new Error("Name must be a string");
+        }
+
+        let maxID = 0;
+        for (let dataPoint of coffeeData) {
+            if (dataPoint.name === name) {
+                throw new Error("name must be unique");
+            }
+            if (dataPoint.id > maxID) {
+                maxID = dataPoint.id;
+            }
+        }
+        const newEntry = {
+            id: `${maxID + 1}`,
+            stamps: 0,
+            freeCoffee: 0,
+            name: name,
+        };
+        coffeeData.push(newEntry);
+        response.json(newEntry);
+    } catch (err) {
+        response.json({ error: err.message });
+    }
+});
+
 //use the environment variable PORT, or 4000 as a fallback
 const PORT = process.env.PORT ?? 4000;
 
@@ -20,8 +57,4 @@ app.listen(PORT, () => {
     console.log(
         `Your express app started listening on ${PORT}, at ${new Date()}`
     );
-});
-
-app.get("/api/data/", (request, response) => {
-    response.json(coffeeData);
 });
