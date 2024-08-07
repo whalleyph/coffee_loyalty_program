866e58c067a77c952a0c60ab215bf5a3eb4c358f
Paul Whalley <81192046+whalleyph@users.noreply.github.com>
2024-08-07 13:18:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Minimal starter for _javascript_ express APIs.
+
+This is a minimal starter template for node.js API apps written in JavaScript (not TypeScript) using Express.
+
+# Change me!
+
+Be sure to change this readme to document your project - it's yours!
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+//allow morgan logger to get access to each request before and after our handlers
+app.use(morgan("dev"));
+//auto-include CORS headers to allow consumption of our content by in-browser js loaded from elsewhere
+app.use(cors());
+//parse body text of requests having content-type application/json, attaching result to `req.body`
+app.use(express.json());
+
+//use the environment variable PORT, or 4000 as a fallback
+const PORT = process.env.PORT ?? 4000;
+
+app.listen(PORT, () => {
+    console.log(
+        `Your express app started listening on ${PORT}, at ${new Date()}`
+    );
+});
